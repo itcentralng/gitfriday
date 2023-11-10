04e09ca74834bfe91306e23227a103fb35ff6607
muhammad <danfulani164@gmaail.com>
2023-11-10 10:18:54 +0100

    friday changes

--- muhammad.py
+++ muhammad.py
@@ -1,2 +1,3 @@
 remark= "Alhamdulilah today is friday"
-prayers='may our lives should be at peace in this world and here after Ameen' 
+prayers='may our lives should be at peace in this world and here after Ameen'
+l="jummat kareem" 
